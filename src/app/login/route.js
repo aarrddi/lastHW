9a78432672bff5fb017b9cb59b2dd7ffa8c70e47
S candel <ardiandro90@gmail.com>
2023-09-09 22:27:16 +0700

    commit pertama

---
+++ src/app/login/route.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../lib/prisma.js";
+
+export const POST = async (req, {params}) =>{
+
+    try{
+
+        const result = await prisma.todo.findmany()
+        console.log(result);
+
+    }catch(err){
+        console.log(err);
+    }
+}
